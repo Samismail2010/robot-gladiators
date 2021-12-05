45e1b6beb0ed65b4fab4e83c2f9a82588d29996b
samismail <samismail2010@gmail.com>
2021-12-05 10:01:40 -0600

    starting JS

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+function fight() {
+   console.log("The fight has begun!"); 
+}
+ //fight(); 
+ var playerName = window.prompt("What is your robot's name?");
+ // Note the lack of quotation marks around the playerName window.alert(playerName);
+ // what is this?
+ console.log(playerName);
+ console.log("This logs a string, good for leaving yourself a message");
+ //this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
